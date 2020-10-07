6538bc0762d372e60cd4073854d04bbdb0d70174
kabilza <naruoluvohina@gmail.com>
2020-10-07 16:09:53 +0700

    done JDBC Lab 7.2

--- src/main/java/th/ac/ku/atm/CustomerService.java
+++ src/main/java/th/ac/ku/atm/CustomerService.java
@@ -8,6 +8,7 @@ import th.ac.ku.atm.data.CustomerRepository;
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CustomerService {
@@ -36,12 +37,12 @@ public class CustomerService {
 
     public Customer findCustomer(int id) {
         try {
-            return repository.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
             return null;
         }
-
     }
+
 
     public Customer checkPin(Customer inputCustomer) {
         // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
