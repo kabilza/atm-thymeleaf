3f530e5d9fad5ce70acb1193583ecc71205aa2e2
kabilza <naruoluvohina@gmail.com>
2020-10-08 10:01:05 +0700

    Done Lab 8 : Part 1

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -54,5 +54,14 @@ public class BankAccountController {
         return "redirect:/bankaccount";
     }
 
+    @PostMapping("/delete/{id}")
+    public String deleteAccount(@PathVariable int id,
+                              @ModelAttribute BankAccount bankAccount,
+                              Model model) {
+
+        bankAccountService.deleteBankAccount(bankAccount);
+        model.addAttribute("bankAccount",bankAccountService.getBankAccounts());
+        return "redirect:/bankaccount";
+    }
 
 }
